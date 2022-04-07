5c98a67803e7928940051f891f905bbb579983b1
Artem <timoty01@ukr.net>
2022-04-07 12:45:48 +0300

    Lesson_14-HW

--- lesson4/src/cron/sendMessageForAllUsers.ts
+++ lesson4/src/cron/sendMessageForAllUsers.ts
@@ -2,17 +2,22 @@ import cron from 'node-cron';
 import { EmailActionEnum } from '../EmailInformation';
 import { userRepositories } from '../repositories';
 import { emailService } from '../services';
+import { IUser } from '../entity';
 
 export const sendMessageForAllUsers = async () => {
     try {
         cron.schedule('0 22 * * 1-5', async () => {
             const users = await userRepositories.getAllUsers();
 
-            users.forEach((user) => {
-                emailService.sendMail(user.email, EmailActionEnum.SEND_SURPRISE_MESSAGE, {
-                    firstName: user.firstName,
-                    lastName: user.lastName,
-                });
+            [...users].map(async (user: IUser) => {
+                try {
+                    await emailService.sendMail(user.email, EmailActionEnum.SEND_SURPRISE_MESSAGE, {
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                    });
+                } catch (e) {
+                    console.log(e);
+                }
             });
         });
     } catch (e) {
