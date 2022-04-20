b1337054c33c89a29db67ad473ca920e6854042a
Artem <timoty01@ukr.net>
2022-04-20 16:45:26 +0300

    Lesson_16-HW

--- lesson4/backend/src/config/config.ts
+++ lesson4/backend/src/config/config.ts
@@ -1,7 +1,3 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 export const config = {
     PORT: process.env.PORT || 5500,
     SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY || 'SECRET_ACCESS_KEY',
